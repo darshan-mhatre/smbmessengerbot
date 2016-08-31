14c39b028ae43ba4c5f8e674f48fbb8face7fb8c
Bhagyashri CG <bhagyashri@cloudganga.com>
2016-08-31 15:03:14 +0530

    sakshi

--- index.js
+++ index.js
@@ -95,27 +95,7 @@ function sendTextMessage(sender, text) {
         method: 'POST',
         json: {
             recipient: { id: sender },
-            message: {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text": "What do you want to do next?",
-                        "buttons": [
-                        {
-                            "type": "web_url",
-                            "url": "https://petersapparel.parseapp.com",
-                            "title": "Show Website"
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Start Chatting",
-                            "payload": "USER_DEFINED_PAYLOAD"
-                        }
-                        ]
-                    }
-                }
-            }
+            message: text
         }
     }, function (error, response, body) {
         if (error) {
