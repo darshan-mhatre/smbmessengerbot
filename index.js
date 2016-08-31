1b64d9cf1aa3f216efcee554d9c3c1739e308e47
Darshan <darshan@cloudganga.com>
2016-08-31 15:13:40 +0530

    Sakshi 2

--- index.js
+++ index.js
@@ -88,6 +88,8 @@ app.post('/webhook/', function (req, res) {
 
 
 function sendTextMessage(sender, text) {
+    console.log(text);
+
     let messageData = { text: text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
