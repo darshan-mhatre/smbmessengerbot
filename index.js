83d0d132b7d110390b700f824cd1663096fc7576
Darshan <darshan@cloudganga.com>
2016-08-31 15:39:14 +0530

    changes

--- index.js
+++ index.js
@@ -88,8 +88,9 @@ app.post('/webhook/', function (req, res) {
 
 
 function sendTextMessage(sender, text) {
-    console.log("started func");
-    console.log("started text"+text);
+
+    console.log("started func sender" +sender);
+    console.log("started text" + text);
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: token },
