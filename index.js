6c7c22ef4a17872f6d5a1a698995b9f9657b87bd
Darshan <darshan@cloudganga.com>
2016-08-31 16:52:00 +0530

    commit

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.post('/webhook/', function (req, res) {
     for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
+    console.log("if message " + event.message.text);
         if (event.message.text === '#book') {
          //   let text = event.message.text
             console.log("if message "+event.message.text);
