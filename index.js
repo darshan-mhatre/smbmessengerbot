7e81098f0e5ad0f3def5abaea08768ff91a4196d
Bhagyashri CG <bhagyashri@cloudganga.com>
2016-08-31 12:37:01 +0530

    up 1

--- index.js
+++ index.js
@@ -43,17 +43,17 @@ app.post('/webhook/', function (req, res) {
             let text = event.message.text
 
             // set content-type header and data as json in args parameter 
-            var args = {
-                data: { "SenderId": sender, "SenderName":senderName, "MsgReceived":text, "Created_Time":crTime },
-                headers: { "Content-Type": "application/json" }
-            };
+            //var args = {
+            //    data: { "SenderId": sender, "SenderName":senderName, "MsgReceived":text, "Created_Time":crTime },
+            //    headers: { "Content-Type": "application/json" }
+            //};
 
-            client.post("http://52.3.172.40/facebookbot/api/Book/SaveDetails", args, function (data, response) {
-                // parsed response body as js object 
-                console.log(data);
-                // raw response 
-                console.log(response);
-            });
+            //client.post("http://52.3.172.40/facebookbot/api/Book/SaveDetails", args, function (data, response) {
+            //    // parsed response body as js object 
+            //    console.log(data);
+            //    // raw response 
+            //    console.log(response);
+            //});
 
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
@@ -62,19 +62,36 @@ app.post('/webhook/', function (req, res) {
 })
 
 function sendTextMessage(sender, text) {
-    client.post("http://52.3.172.40/facebookbot/api/Book/GetBookCategories", args, function (data, response) {
-        bookCategory = response;
-    });
-    let messageData = { text:text }
+    let messageData = { text: text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: { access_token: token },
         method: 'POST',
         json: {
-            recipient: {id:sender},
-            message: bookCategory
+            recipient: { id: sender },
+            message: {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "button",
+                        "text": "What do you want to do next?",
+                        "buttons": [
+                        {
+                            "type": "web_url",
+                            "url": "https://petersapparel.parseapp.com",
+                            "title": "Show Website"
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Start Chatting",
+                            "payload": "USER_DEFINED_PAYLOAD"
+                        }
+                        ]
+                    }
+                }
+            }
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -82,6 +99,29 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+
+//function sendTextMessage(sender, text) {
+
+//    client.post("http://52.3.172.40/facebookbot/api/Book/GetBookCategories", args, function (data, response) {
+//        bookCategory = response;
+//    });
+//    let messageData = { text:text }
+//    request({
+//        url: 'https://graph.facebook.com/v2.6/me/messages',
+//        qs: {access_token:token},
+//        method: 'POST',
+//        json: {
+//            recipient: {id:sender},
+//            message: bookCategory
+//        }
+//    }, function(error, response, body) {
+//        if (error) {
+//            console.log('Error sending messages: ', error)
+//        } else if (response.body.error) {
+//            console.log('Error: ', response.body.error)
+//        }
+//    })
+//}
 
 const token = "EAABuCopCejMBAEEr1uprVLUSzvHCDLgGUrfZCyTy0qdQbs2yjdA2vDjkJUQmvm3EcCiW9fyRgJqs9KfTGZBnxn8ZA0ISyW1Athf7IboqZC8zzT59xOa169BNV0SmNKcOuHL2zDFotVMcw6IM6JQXEVOIt3WH4WgZBvURHd1PPzwZDZD"
 
