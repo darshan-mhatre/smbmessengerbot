39cadd19f81ac2dba88aa2a4cebf263c1fda862d
Bhagyashri CG <bhagyashri@cloudganga.com>
2016-08-31 14:46:54 +0530

    changed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging;
     console.log("test");
    // for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[0]
+    let event = req.body.entry[0].messaging
     let sender = event.sender.id
         //if (event.message && event.message.text) {
          //   let text = event.message.text
