dc767d0704c8e752b6ca10a4567fd94ec432cfff
Darshan <darshan@cloudganga.com>
2016-08-31 15:27:56 +0530

    changes

--- index.js
+++ index.js
@@ -94,24 +94,28 @@ function sendTextMessage(sender, text) {
         method: 'POST',
         json: {
             recipient: { id: sender },
-            message: {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text": "What do you want to do next?",
-                        "buttons": [{
-                            "type": "postback",
-                            "title": "Business",
-                            "payload": "1"
-                        }, {
-                            "type": "postback",
-                            "title": "Sports",
-                            "payload": "2"
-                        }]
-                    }
-                }
-            }
+            message: text
+        //{
+            //    "attachment": {
+            //        "type": "template",
+            //        "payload": {
+            //            "template_type": "button",
+            //            "text": "What do you want to do next?",
+            //            "buttons": [
+            //            {
+            //                "type": "web_url",
+            //                "url": "https://petersapparel.parseapp.com",
+            //                "title": "Show Website"
+            //            },
+            //            {
+            //                "type": "postback",
+            //                "title": "Start Chatting",
+            //                "payload": "USER_DEFINED_PAYLOAD"
+            //            }
+            //            ]
+            //        }
+            //    }
+            //}
         }
     }, function (error, response, body) {
         if (error) {
