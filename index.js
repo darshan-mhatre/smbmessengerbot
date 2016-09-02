cfc1eaa5a616f48ddac5ae6c035fa368e954df4d
Darshan <darshan@cloudganga.com>
2016-09-02 11:28:06 +0530

    or

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 'use strict'
-//Constants
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-
-//Example POST method invocation 
-var Client = require('node-rest-client').Client; 
-var client = new Client();
-
 const app = express()
+
 app.set('port', (process.env.PORT || 5000))
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// Process application/json
+// parse application/json
 app.use(bodyParser.json())
 
-// Index route
+// index
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('hello world i am a secret bot')
 })
 
-// for Facebook verification
+// for facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
@@ -30,7 +26,7 @@ app.get('/webhook/', function (req, res) {
     res.send('Error, wrong token')
 })
 
-
+// to post data
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
@@ -38,55 +34,29 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            let text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
+            continue
         }
     }
     res.sendStatus(200)
-
-    //let requ = req
-    //console.log("req = " + requ)
-    //let bodyu  = req.body
-    //console.log("body = " + bodyu)
-    //let entrys = req.body.entry[0]
-    //console.log("entry = " + entrys)
-    //let messaging_events = req.body.entry[0].messaging
-    //console.log("before for = " + messaging_events)
-    //for (let i = 0; i < messaging_events.length; i++) {
-    //    console.log("in for = ")
-    //    let event = req.body.entry[0].messaging[i]
-    //    let sender = event.sender.id
-    //    if (event.message && event.message.text) {
-    //        console.log("in if = " + event.message.text)
-    //        let text = event.message.text
-
-    //        // set content-type header and data as json in args parameter 
-
-    //        var args = {
-    //            data: {},
-    //            headers: { "Content-Type": "application/json" }
-    //        };
-
-    //        client.post("http://52.3.172.40/facebookbot/api/Book/GetBookCategories", args, function (data, response) {
-    //            // parsed response body as js object 
-    //            console.log(data)
-    //            // raw response 
-    //            console.log(response)
-    //            sendTextMessage(sender, data.message)
-    //        });
-
-    //        // sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-    //    }
-    //    else
-    //    {
-
-    //        console.log("else condition = " + messaging_events)
-    //    }
-    //}
-    //res.sendStatus(200)
 })
+
+
+// recommended to inject access tokens as environmental variables, e.g.
+// const token = process.env.PAGE_ACCESS_TOKEN
+const token = "EAABuCopCejMBAEEr1uprVLUSzvHCDLgGUrfZCyTy0qdQbs2yjdA2vDjkJUQmvm3EcCiW9fyRgJqs9KfTGZBnxn8ZA0ISyW1Athf7IboqZC8zzT59xOa169BNV0SmNKcOuHL2zDFotVMcw6IM6JQXEVOIt3WH4WgZBvURHd1PPzwZDZD"
 
 function sendTextMessage(sender, text) {
     let messageData = { text: text }
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: token },
@@ -104,76 +74,56 @@ function sendTextMessage(sender, text) {
     })
 }
 
+function sendGenericMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
-//function sendTextMessage(sender, text) {
-
-//    console.log("started func sender" +sender)
-//    console.log("started text" + text)
-//    request({
-//        url: 'https://graph.facebook.com/v2.6/me/messages',
-//        qs: { access_token: token },
-//        method: 'POST',
-//        json: {
-//            recipient: { id: sender },
-//            message: text
-//        //{
-//            //    "attachment": {
-//            //        "type": "template",
-//            //        "payload": {
-//            //            "template_type": "button",
-//            //            "text": "What do you want to do next?",
-//            //            "buttons": [
-//            //            {
-//            //                "type": "web_url",
-//            //                "url": "https://petersapparel.parseapp.com",
-//            //                "title": "Show Website"
-//            //            },
-//            //            {
-//            //                "type": "postback",
-//            //                "title": "Start Chatting",
-//            //                "payload": "USER_DEFINED_PAYLOAD"
-//            //            }
-//            //            ]
-//            //        }
-//            //    }
-//            //}
-//        }
-//    }, function (error, response, body) {
-//        if (error) {
-//            console.log('Error sending messages: ', error)
-//        } else if (response.body.error) {
-//            console.log('Error: ', response.body.error)
-//        }
-//    })
-//}
-
-//function sendTextMessage(sender, text) {
-
-//    client.post("http://52.3.172.40/facebookbot/api/Book/GetBookCategories", args, function (data, response) {
-//        bookCategory = response;
-//    });
-//    let messageData = { text:text }
-//    request({
-//        url: 'https://graph.facebook.com/v2.6/me/messages',
-//        qs: {access_token:token},
-//        method: 'POST',
-//        json: {
-//            recipient: {id:sender},
-//            message: bookCategory
-//        }
-//    }, function(error, response, body) {
-//        if (error) {
-//            console.log('Error sending messages: ', error)
-//        } else if (response.body.error) {
-//            console.log('Error: ', response.body.error)
-//        }
-//    })
-//}
-
-const token = "EAABuCopCejMBAEEr1uprVLUSzvHCDLgGUrfZCyTy0qdQbs2yjdA2vDjkJUQmvm3EcCiW9fyRgJqs9KfTGZBnxn8ZA0ISyW1Athf7IboqZC8zzT59xOa169BNV0SmNKcOuHL2zDFotVMcw6IM6JQXEVOIt3WH4WgZBvURHd1PPzwZDZD"
-
-// Spin up the server
-app.listen(app.get('port'), function() {
+// spin spin sugar
+app.listen(app.get('port'), function () {
     console.log('running on port', app.get('port'))
 })
-
