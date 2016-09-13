62fea2c43a4c6a5eb16c8340ec29643b6a3cf651
Darshan <darshan@cloudganga.com>
2016-09-13 16:31:12 +0530

    books

--- index.js
+++ index.js
@@ -158,8 +158,12 @@ function sendTextMessageOnResponseAPI(sender, text) {
 }
 
 function sendGenericMessage(sender) {
-
-    let messageData = { "attachment": { "type": "template", "payload": { "template_type": "generic", "elements": [{ "title": "pqr", "subtitle": "test  2 description 1", "image_url": "", "buttons": [{ "type": "postback", "title": "Postback", "payload": "Payload for first element in a generic bubble", }] }, { "title": "hgfshd", "subtitle": "nabdmsnfd", "image_url": "", "buttons": [{ "type": "postback", "title": "Postback", "payload": "Payload for second element in a generic bubble", }] }] } } }
+    var myObj = { "attachment": { "type": "template", "payload": { "template_type": "generic", "elements": [{ "title": "pqr", "subtitle": "test  2 description 1", "image_url": "", "buttons": [{ "type": "postback", "title": "Postback", "payload": "Payload for first element in a generic bubble", }] }, { "title": "hgfshd", "subtitle": "nabdmsnfd", "image_url": "", "buttons": [{ "type": "postback", "title": "Postback", "payload": "Payload for second element in a generic bubble", }] }] } } }
+    delete myObj.attachment.payload["text"];
+    console.log('text delete ', myObj)
+    delete myObj.attachment.payload["buttons"];
+    console.log('buttons delete ', myObj)
+    let messageData = myObj
         //{ "attachment": { "type": "template", "payload": { "template_type": "generic", "elements": [{ "title": "pqr", "subtitle": "test  2 description 1", "image_url": "", "buttons": [{ "type": "postback", "title": "Postback", "payload": "Payload for first element in a generic bubble", }] }, { "title": "hgfshd", "subtitle": "nabdmsnfd", "image_url": "", "buttons": [{ "type": "postback", "title": "Postback", "payload": "Payload for second element in a generic bubble", }] }] } } }
     //let messageData = {
     //    "attachment": {
