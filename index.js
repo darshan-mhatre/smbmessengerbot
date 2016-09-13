1519d4c89156c591b2a7d866165f19c6a25e43e7
Darshan <darshan@cloudganga.com>
2016-09-13 11:38:33 +0530

    Changes

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/webhook/', function (req, res) {
 // to post data
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
+    console.log("before for condition = ")
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
