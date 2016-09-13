940d856ac560a8e858ed8b33991f4803ea283d93
Darshan <darshan@cloudganga.com>
2016-09-13 12:59:13 +0530

    book details

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ function sendTextMessageOnResponseAPI(sender, text) {
 
 function sendGenericMessage(sender) {
 
-    let messageData = { "attachment": { "type": "template", "payload": { "template_type": "generic", "text": null, "buttons": null, "elements": [{ "title": "pqr", "subtitle": "test  2 description 1", "image_url": "", "buttons": null }, { "title": "hgfshd", "subtitle": "nabdmsnfd", "image_url": "", "buttons": null }] } } }
+    let messageData = { "attachment": { "type": "template", "payload": { "template_type": "generic", "elements": [{ "title": "pqr", "subtitle": "test  2 description 1", "image_url": "", "buttons": null }, { "title": "hgfshd", "subtitle": "nabdmsnfd", "image_url": "", "buttons": null }] } } }
     //let messageData = {
     //    "attachment": {
     //        "type": "template",
