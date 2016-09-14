7f76bbc71fee44b04b365c859bacb2e0a254d77a
rajeshshirkecg <rajesh.shirke@cloudgaga.com>
2016-09-14 11:09:51 +0530

    adasd

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.get('/webhook', function (req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
+  console.log("req.body");
+  console.log(req.body);
   if (data.object == 'page') {
     data.entry.forEach(function (pageEntry) {
       var pageID = pageEntry.id;
