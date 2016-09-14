00fb94b11a5fe93ae7b3c3245d0eee9372080760
rajeshshirkecg <rajesh.shirke@cloudgaga.com>
2016-09-14 12:10:11 +0530

    asdasdas

--- index.js
+++ index.js
@@ -1,40 +1,88 @@
-var app = require('express')();
-var bodyParser  = require('body-parser');
-var request = require('request');
+'use strict'
+//Constants
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.set('port', (process.env.PORT || 5000))
 
-var port = process.env.PORT || 8080;
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
-var token = "EAABuCopCejMBAHqFn3lvKbH3FcHV8KMFBKTNPETuE1ZABN4BA2zzhMCoI4oAEKSVkpODzsJJdK0DOjHeL0rZCl8MWg4nZBiRoMoGNwLPZC2l6fuhp67ZCuV1YfNcX1S1Op2EY9JY3MHaBl817YdMwZBmGO2CfNgrTfWtttfMhiYAZDZD";
+// Process application/json
+app.use(bodyParser.json())
 
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'test smb bot') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
-});
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
 
 app.post('/webhook/', function (req, res) {
-  var messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      //Do whatever you want with the text
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
-  }
-  res.sendStatus(200);
-});
+    res.sendStatus(200)
+})
 
-app.get('/', function (req, res) {
-  res.sendStatus(200);
-});
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: {
+                        "attachment":{
+                        "type":"template",
+                        "payload":{
+                            "template_type":"button",
+                            "text":"What do you want to do next?",
+                            "buttons":[
+                            {
+                                "type":"web_url",
+                                "url":"https://petersapparel.parseapp.com",
+                                "title":"Show Website"
+                            },
+                            {
+                                "type":"postback",
+                                "title":"Start Chatting",
+                                "payload":"USER_DEFINED_PAYLOAD"
+                            }
+                            ]
+                        }
+                        }
+            }
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
-app.listen(port, function () {
-  console.log('The webhook is running on port ' + port);
-});
+const token = "EAABuCopCejMBAEEr1uprVLUSzvHCDLgGUrfZCyTy0qdQbs2yjdA2vDjkJUQmvm3EcCiW9fyRgJqs9KfTGZBnxn8ZA0ISyW1Athf7IboqZC8zzT59xOa169BNV0SmNKcOuHL2zDFotVMcw6IM6JQXEVOIt3WH4WgZBvURHd1PPzwZDZD"
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
