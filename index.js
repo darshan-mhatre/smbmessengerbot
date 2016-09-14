95bce71bcc7cc4f6053cfc261e74b343b273d5a9
rajeshshirkecg <rajesh.shirke@cloudgaga.com>
2016-09-14 11:28:43 +0530

    asd asdasd

--- index.js
+++ index.js
@@ -1,103 +1,40 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var app = require('express')();
+var bodyParser  = require('body-parser');
 var request = require('request');
-var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var port = process.env.PORT || 8080;
+
 var token = "EAABuCopCejMBAEEr1uprVLUSzvHCDLgGUrfZCyTy0qdQbs2yjdA2vDjkJUQmvm3EcCiW9fyRgJqs9KfTGZBnxn8ZA0ISyW1Athf7IboqZC8zzT59xOa169BNV0SmNKcOuHL2zDFotVMcw6IM6JQXEVOIt3WH4WgZBvURHd1PPzwZDZD";
 
-app.set('port', (process.env.PORT || 1000))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      //Do whatever you want with the text
+    }
+  }
+  res.sendStatus(200);
+});
 
 app.get('/', function (req, res) {
-  res.send('Facebook Bot')
+  res.sendStatus(200);
 });
 
-/*
- * Use your own validation token. Check that the token used in the Webhook 
- * setup is the same token used here.
- *
- */
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
+app.listen(port, function () {
+  console.log('The webhook is running on port ' + port);
 });
-
-/*
- * All callbacks for Messenger are POST-ed. They will be sent to the same webhook.
- *
- */
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-  console.log("req.body");
-  console.log(req.body);
-  console.log("data.object");
-  console.log(data.object);
-  if (data.object == 'page') {
-    data.entry.forEach(function (pageEntry) {
-
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
-  console.log("pageID");
-  console.log(pageEntry);
-      pageEntry.messaging.forEach(function (event) {
-        if (event.message && event.message.text) {
-  console.log("event");
-  console.log(event);
-          receivedMessage(event);
-          
-        }
-      });
-    });
-    res.sendStatus(200);
-  }
-});
-
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message.text;
-
-  var messageText = "I'm Siva, How can I help you ?";
-
-  var messageData = {
-    recipient: { id: senderID },
-    message: { text: messageText }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Call the Send API. The message data goes in the body. If successful, we'll 
- * get the message id in a response 
- *
- */
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: token },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-      console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
-}
-
-app.listen(app.get('port'), function () {
-  console.log('running on port', app.get('port'))
-})
