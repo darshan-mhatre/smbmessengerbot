4d4ab2b2c2befcdd3b580ab11dfdf9f10f184c14
rajeshshirkecg <rajesh.shirke@cloudgaga.com>
2016-09-14 11:06:26 +0530

    zf sdfdsfdsfsd

--- index.js
+++ index.js
@@ -1,107 +1,93 @@
-'use strict'
-//Constants
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+var token = "EAABuCopCejMBAEEr1uprVLUSzvHCDLgGUrfZCyTy0qdQbs2yjdA2vDjkJUQmvm3EcCiW9fyRgJqs9KfTGZBnxn8ZA0ISyW1Athf7IboqZC8zzT59xOa169BNV0SmNKcOuHL2zDFotVMcw6IM6JQXEVOIt3WH4WgZBvURHd1PPzwZDZD";
 
-//Example POST method invocation 
-var Client = require('node-rest-client').Client; 
-var client = new Client();
-
-const app = express()
-
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
+app.set('port', (process.env.PORT || 1000))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
+  res.send('Facebook Bot')
+});
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-})
+/*
+ * Use your own validation token. Check that the token used in the Webhook 
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
 
-
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same webhook.
+ *
+ */
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+  if (data.object == 'page') {
+    data.entry.forEach(function (pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+      pageEntry.messaging.forEach(function (event) {
         if (event.message && event.message.text) {
-            let text = event.message.text
-
-            // set content-type header and data as json in args parameter 
-            var args = {
-                data: { "BookCategoryID":1 },
-                headers: { "Content-Type": "application/json" }
-            };
-
-            client.post("http://52.3.172.40/facebookbot/api/Book/GetBooks", args, function (data, response) {
-                // parsed response body as js object 
-                console.log(data);
-                // raw response 
-                console.log(response);
-            });
-
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+          receivedMessage(event);
         }
-    }
-    res.sendStatus(200)
-})
+      });
+    });
+    res.sendStatus(200);
+  }
+});
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: {
-                        "attachment":{
-                        "type":"template",
-                        "payload":{
-                            "template_type":"button",
-                            "text":"What do you want to do next?",
-                            "buttons":[
-                            {
-                                "type":"web_url",
-                                "url":"https://petersapparel.parseapp.com",
-                                "title":"Show Website"
-                            },
-                            {
-                                "type":"postback",
-                                "title":"Start Chatting",
-                                "payload":"USER_DEFINED_PAYLOAD"
-                            }
-                            ]
-                        }
-                        }
-            }
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message.text;
+
+  var messageText = "I'm Siva, How can I help you ?";
+
+  var messageData = {
+    recipient: { id: senderID },
+    message: { text: messageText }
+  };
+
+  callSendAPI(messageData);
 }
 
-const token = "EAABuCopCejMBAEEr1uprVLUSzvHCDLgGUrfZCyTy0qdQbs2yjdA2vDjkJUQmvm3EcCiW9fyRgJqs9KfTGZBnxn8ZA0ISyW1Athf7IboqZC8zzT59xOa169BNV0SmNKcOuHL2zDFotVMcw6IM6JQXEVOIt3WH4WgZBvURHd1PPzwZDZD"
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll 
+ * get the message id in a response 
+ *
+ */
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: token },
+    method: 'POST',
+    json: messageData
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+      console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'))
 })
-
