6093da4d924da946672878f20189f8a21f03e23b
Darshan <darshan@cloudganga.com>
2016-09-21 13:49:42 +0530

    ygjkkiukciuc

--- index.js
+++ index.js
@@ -128,33 +128,7 @@ function testFunc(sender, text) {          // testFunction
         method: 'POST',
         json: {
             recipient: { id: sender },
-            message:  {
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "generic",
-                            "elements": [{
-                                "title": "First card",
-                                "subtitle": "Element #1 of an hscroll",
-                                "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                                "buttons": [{
-                                    "type": "web_url",
-                                    "url": "https://www.messenger.com",
-                                    "title": "web url"
-                                }],
-                            }, {
-                                "title": "Second card",
-                                "subtitle": "Element #2 of an hscroll",
-                                "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                                "buttons": [{
-                                    "type": "postback",
-                                    "title": "Postback",
-                                    "payload": "Payload for second element in a generic bubble",
-                                }],
-                            }]
-                        }
-                    }
-                }
+            message: { "attachment": { "type": "template", "payload": { "template_type": "generic", "elements": [{ "title": "pqr", "subtitle": "test  2 description 1", "image_url": "", "buttons": { "type": "postback", "title": "Order book", "payload": "3" } }, { "title": "hgfshd", "subtitle": "nabdmsnfd", "image_url": "", "buttons": { "type": "postback", "title": "Order book", "payload": "5" } }] } } } 
         }
     }, function (error, response, body) {
         if (error) {
