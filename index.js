d04b98bc6a40a1f98362129282fbd637630295fc
Darshan <darshan@cloudganga.com>
2016-09-27 16:22:27 +0530

    fgjgkh

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ app.post('/webhook/', function (req, res) {
             if (str.length > 7 && str.substring(0, 6) == "bookId") {
                 var id = str.slice(7);
                 console.log("slice id = ", id)
+                console.log("save order userId : ",userId)
                 var param = { "UserID": userId, "BookID": id }
                 callApi("Book/SaveBookOrder", param, function (data) {                  // call get book api
                     console.log('get response book: ', data.message + data.OrderID)
